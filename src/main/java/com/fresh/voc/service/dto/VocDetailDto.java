29bd31903953baa0d45f2c1ce9d72844133a22fd
midasworld <midas117@knou.ac.kr>
2023-05-24 03:52:22 +0900

    feat: 배상 목록 조회 API - Spec 변경(패널티 정보 추가)

--- src/main/java/com/fresh/voc/service/dto/VocDetailDto.java
+++ src/main/java/com/fresh/voc/service/dto/VocDetailDto.java
@@ -4,6 +4,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import com.fresh.voc.model.common.Person;
 import com.fresh.voc.model.voc.DueType;
+import com.fresh.voc.model.voc.Penalty;
 import com.fresh.voc.model.voc.Voc;
 
 import lombok.Getter;
@@ -14,17 +15,25 @@ public class VocDetailDto {
 	private final DueType dueType;
 	private final String dueReason;
 	private String dueTargetName;
+	private String penaltyContent;
+	private Boolean confirmed;
+	private Boolean objected;
 
 	public VocDetailDto(Voc voc) {
-		this(voc, voc.getDueTarget());
+		this(voc, voc.getDueTarget(), voc.getPenalty());
 	}
 
-	public VocDetailDto(Voc voc, Person person) {
+	public VocDetailDto(Voc voc, Person person, Penalty penalty) {
 		this.id = voc.getId();
 		this.dueType = voc.getDueType();
 		this.dueReason = voc.getDueReason();
 		if (person != null) {
 			this.dueTargetName = person.getName();
+		}
+		if (penalty != null) {
+			this.penaltyContent = penalty.getContent();
+			this.confirmed = penalty.getConfirmed();
+			this.objected = penalty.getObjected();
 		}
 	}
 
@@ -35,6 +44,9 @@ public class VocDetailDto {
 			.append("dueType", dueType)
 			.append("dueReason", dueReason)
 			.append("dueTargetName", dueTargetName)
+			.append("penaltyContent", penaltyContent)
+			.append("confirmed", confirmed)
+			.append("objected", objected)
 			.toString();
 	}
 }
